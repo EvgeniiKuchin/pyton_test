083e1302fc0105d2261707b2cfffab17fc5333bc
evgenijkucin <neon-power2@yandex.ru>
2022-12-19 21:47:51 +0300

    add file

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-5j181+-q62x+9m9yh#&%)%&8#^dhs^&%!hzs_l=fl=rh(6zr$p'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = {'natilashes.ru', 'www.natilashes.ru'}
 
